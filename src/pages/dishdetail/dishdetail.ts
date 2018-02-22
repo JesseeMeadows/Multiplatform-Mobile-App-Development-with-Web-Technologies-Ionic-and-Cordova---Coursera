2b238a7bb606bf46f8b6dac8848570584b5e17f4
JesseeMeadows <jesseemeadows@gmail.com>
2018-02-22 06:47:49 -0500

    Assignment 2

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,8 +1,9 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, ActionSheetController, ModalController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
+import { CommentPage } from '../comment/comment';
 
 @IonicPage()
 @Component({
@@ -19,6 +20,8 @@ export class DishdetailPage {
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private favoriteservice: FavoriteProvider,
     private toastCtrl: ToastController,
+    private actionsheetCtrl: ActionSheetController,
+    private modalCtrl: ModalController,
     @Inject('BaseURL') private BaseURL) {
 
       this.dish = navParams.get('dish');
@@ -27,11 +30,6 @@ export class DishdetailPage {
       let total = 0;
       this.dish.comments.forEach(comment => total += comment.rating);
       this.avgstars = (total / this.numcomments).toFixed(2);
-
-      this.toastCtrl.create({
-        message: 'Dish ' + this.dish.id + ' added as favorite successfully',
-        position: 'middle',
-        duration: 3000}).present();
   }
 
   ionViewDidLoad() {
@@ -45,6 +43,50 @@ export class DishdetailPage {
       message: 'Dish ' + this.dish.id + ' added as favorite successfully',
       position: 'middle',
       duration: 3000}).present();
+  }
+
+  openDishOptions() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: "Select Action",
+      cssClass: "no-scroll",
+      buttons: [
+        {
+          text: 'Add to Favorites',
+          handler: () => {
+            this.addToFavorites();
+          }
+        },
+        {
+          text: "Add Comment",
+          handler: () => {
+            this.openCommentForm();
+          }
+
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Delete cancelled');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  openCommentForm() {
+    let modal = this.modalCtrl.create(CommentPage);
+    modal.onDidDismiss(comment => {
+      if (comment) {
+        this.dish.comments.push(comment);
+        this.numcomments = this.dish.comments.length;
+        let total = 0;
+        this.dish.comments.forEach(comment => total += comment.rating);
+        this.avgstars = (total / this.numcomments).toFixed(2);
+      }
+    })
+    modal.present();
   }
 
 }
