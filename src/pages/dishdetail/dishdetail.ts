dd938724f033c03c8dc6dbd55154bd3c73e848a3
JesseeMeadows <jesseemeadows@gmail.com>
2018-02-28 07:59:36 -0500

    Social Sharing

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -4,6 +4,7 @@ import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
 import { CommentPage } from '../comment/comment';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @IonicPage()
 @Component({
@@ -22,6 +23,7 @@ export class DishdetailPage {
     private toastCtrl: ToastController,
     private actionsheetCtrl: ActionSheetController,
     private modalCtrl: ModalController,
+    private socialSharing: SocialSharing,
     @Inject('BaseURL') private BaseURL) {
 
       this.dish = navParams.get('dish');
@@ -62,6 +64,22 @@ export class DishdetailPage {
             this.openCommentForm();
           }
 
+        },
+        {
+          text: 'Share via Facebook',
+          handler: () => {
+            this.socialSharing.shareViaFacebook(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Facebook'))
+              .catch(() => console.log('Failed to post to Facebook'));
+          }
+        },
+        {
+          text: 'Share via Twitter',
+          handler: () => {
+            this.socialSharing.shareViaTwitter(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Twitter'))
+              .catch(() => console.log('Failed to post to Twitter'));
+          }
         },
         {
           text: 'Cancel',
